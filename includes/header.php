99c41c9d54e8a8d5fa9dc47191c59a5d8d51e90c
Daron Howard <39311740+Dazzler1970@users.noreply.github.com>
2019-03-06 15:06:04 +0000

    footer.php

---
+++ includes/header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords"
+        content="accounts, business, administration services, payroll, bookkeeping, personnel, human resources, management, invoice, invoicing, staff, pension">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css"
+        integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+    <script defer src="https://use.fontawesome.com/releases/v5.4.2/js/all.js"
+        integrity="sha384-wp96dIgDl5BLlOXb4VMinXPNiB32VYBSoXOoiARzSTXY+tsK8yDTYfvdTyqzdGGN"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="pagestyles.css">
+    <link rel="stylesheet" href="boxestyles.css">
+    <link rel="stylesheet" href="mediaq.css">
+    <link rel="stylesheet" href="gridstyles.css">
+
+    <title>Business Administration Services - McHoward's Back-Office</title>
+
+
+    <!-- Control Wrapper -->
+    <div class="Wrapper">
+        <!-- The social media icon bar -->
+        <div class="icon-bar">
+            <a href="https://www.facebook.com">
+                <i class="fab fa-facebook-square fa-2x" class="facebook"></i>
+            </a>
+            <a href="https://twitter.com">
+                <i class="fab fa-twitter-square fa-2x" class="twitter"></i>
+            </a>
+            <a href="https://accounts.google.com">
+                <i class="fab fa-google-plus-square fa-2x" class="google" alt="google-plus"></i>
+            </a>
+            <a href="https://www.linkedin.com">
+                <i class="fab fa-linkedin fa-2x" class="linkedin" alt="linkedin"></i>
+
+            </a>
+            <a href="https://www.youtube.co.uk">
+                <i class="fab fa-youtube-square fa-2x" class="youtube" alt="YouTube"></i>
+            </a>
+        </div>
+        <!-- Navigation -->
+        <!--Navigation Buttons-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <li><a class="nav-item nav-link" href="home.html">Home</a></li>
+                    <li><a class="nav-item nav-link" href="accounts.html"></i>Bookkeeping</a></li>
+                    <li><a class="nav-item nav-link" href="wordprocessing.html">Document Processing</a></li>
+                    <li><a class="nav-item nav-link" href="payroll.html">Payroll</a></li>
+                    <li><a class="nav-item nav-link" href="personnel.html">Personnel</a></li>
+                    <li><a class="nav-item nav-link" href="contactus.html">Contact</a></li>
+                </div>
+            </div>
+            <div class="topnav">
+                <div class="search-container">
+                    <form action="/action_page.php">
+                        <input type="text" placeholder="Search.." name="search">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </head>
