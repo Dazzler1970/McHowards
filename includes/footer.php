5e2c77b3f6abde8502139ceed46294d1324c4d72
Daron Howard <39311740+Dazzler1970@users.noreply.github.com>
2019-03-06 15:36:54 +0000

    Correction Template

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,2 @@
 <h1>Footer</h1>
+<li>&copy;2019 D. Howard</li>
--- templates/template1.php
+++ templates/template1.php
@@ -3,21 +3,18 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>&copy;2019 D. Howard</title>
+    <title>Template 2019 D. Howard</title>
 </head>
 <body>
 <header>
 <?php include 'includes\header.php';?>
-Hello
 </header>
 <nav>
 <?php include 'includes\navigation.php';?>
-Navigation
 </nav>
 </main></main>
 <footer>
 <?php include 'includes\footer.php';?>
-Goodbye
 </footer>
 
 
