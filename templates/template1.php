c3c8691f7419187b0add2af0e93fa31b79de3168
Daron Howard <39311740+Dazzler1970@users.noreply.github.com>
2019-03-06 19:01:24 +0000

    pasted

--- templates/template1.php
+++ templates/template1.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>D. Howard</title>
-</head>
-<body>
-<header>
-<?php include 'includes\header.html';?>
-</header>
-<nav>
-<?php include 'includes\navigation.html';?>
-</nav>
-</main></main>
-<footer>
-<?php include 'includes\footer.html';?>
-</footer>
-</body>
+<<!DOCTYPE html>
+<html>
+   <head>
+       <link rel="stylesheet" type="text/css" href="style.css">
+   </head>
+   <body>
+       <?php include 'header.html'; ?>
+       <?php include 'footer.html'; ?>
+   </body>
+</html>
