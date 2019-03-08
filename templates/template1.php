a9b64bb4a598628d8a45beed7d52dc7cd6aa4a16
Daron Howard <39311740+Dazzler1970@users.noreply.github.com>
2019-03-08 13:07:47 +0000

    styles

--- templates/template1.php
+++ templates/template1.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
    <head>
-       <link rel="stylesheet" type="text/css" href="style.css">
+       <link rel="stylesheet" href="styles.css">
    </head>
    <body>
        <?php include 'header.html'; ?>
